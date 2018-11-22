d984ef0571537dc71ce6438e57c854458cc4459f
StilllFantasy <1784788211@qq.com>
2018-11-22 10:05:06 +0800

    wtf?!

--- WaterProblem/abc.cpp
+++ WaterProblem/abc.cpp
@@ -1,45 +1,20 @@
 #include<iostream>
 using namespace std;
-#define max 100000
-typedef long long ll;
-int n,k;
-ll t[max];
-int check(ll p)
-{
-    int i = 0;
-    for (int j = 0; j < k; j++)
-    {
-        ll s = 0;
-        while (s + t[i] <= p)
-        {
-            s += t[i];
-            i++;
-            if (i == n) return n;
-        }
-    }
-    return i;
-}
-int solve()
-{
-    ll left = 0;
-    ll right = max * max;
-    ll mid ;
-    while (right - left > 1)
-    {
-        mid = (left + right)/2;
-        int v = check(mid);
-        if(v >= n)
-        right = mid;
-        else left = mid;
-    }
-    return right;
-}
 int main()
 {
-    cin>>n>>k;
-    for (int i = 0; i < n; i++)
-    cin>>t[i];
-    ll ans = solve();
-    cout<<ans;
+    int k = 0;
+    int sum = 0;
+    int i=1;
+    while(i <= 101)
+    {
+        if(k%2==0)
+            sum-=i;
+        else
+            sum+=i;
+        i+=2;
+        k++;
+    }
+    cout<<sum;
+    
     return 0;
 }
