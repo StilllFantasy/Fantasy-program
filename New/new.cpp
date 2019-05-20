6ef411e8fabbfb18cf124dec39f3c4f842f5a975
StilllFantasy <1784788211@qq.com>
2019-05-20 14:05:28 +0800

    19.5.20

--- New/new.cpp
+++ New/new.cpp
@@ -1,27 +1,86 @@
 #include <iostream>
-#include <cmath>
-#include <algorithm>
+#include <queue>
+#include <cstring>
 using namespace std;
-int n, m;
-int a[100002];
-int b[100002];
-int c[100002];
+int n, m, k, ans;
+int mchx[1001];
+int mchy[1001];
+int visx[1001];
+int visy[1001];
+vector<int> G[1001];
+int dfs(int x)
+{
+    for (int i = 0; i < G[x].size(); i++)
+    {
+        int v = G[x][i];
+        if (!visy[v])
+        {
+            visy[v] = 1;
+            if (!mchy[v] || dfs(mchy[v]))
+            {
+                mchy[v] = x;
+                mchx[x] = v;
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+int dfs2(int x)
+{
+    visx[x] = 1;
+    for (int i = 0; i < G[x].size(); i++)
+    {
+        int v = G[x][i];
+        if (!visy[v])
+        {
+            visy[v] = 1;
+            if (!mchy[v] || dfs2(mchy[v]))
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 int main()
 {
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    while (cin >> n >> m >> k)
     {
-        cin >> a[i];
-        b[i] = 1;
-    }
-    for (int i = 1; i <= n; i++)
-        for (int j = i + 1; j <= n; j++)
+        for (int i = 0; i < k; i++)
         {
-            if (a[j] < a[i])
-                b[j] = max(b[j], b[i] + 1);  
+            int a, b;
+            cin >> a >> b;
+            G[a].push_back(b);
         }
-    for (int i = 1; i <= n; i++)
-        m = max(m, b[i]);
-    cout << m;
-    return 0;
+        for (int i = 1; i <= n; i++)
+        {
+            memset(visy, 0, sizeof(visy));
+            if (dfs(i))
+                ans++;
+        }
+        memset(visy, 0, sizeof(visy));
+        for (int i = 1; i <= n; i++)
+        {
+            if (!mchx[i])
+                dfs2(i);
+        }
+        cout << ans << " ";
+        for (int i = 1; i <= n; i++)
+            if (!visx[i])
+                cout << "r" << i << " ";
+        for (int i = 1; i <= m; i++)
+            if (visy[i])
+                cout << "c" << i << " ";
+        cout << endl;
+        for(int i=1;i<=n;i++)
+        {
+            mchx[i]=0;
+            mchy[i]=0;
+            visx[i]=0;
+            visy[i]=0;
+            G[i].clear();
+            ans=0;
+        }
+    }
 }
